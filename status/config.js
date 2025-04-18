556e00cfe59d233c04475669c6985173307cdc36
unknown <1071408545@qq.com>
2025-04-19 03:50:17 +0800

    first commit

---
+++ status/config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '站点监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2620953-1c3ddc98179d973314b8ae62',
+  ],
+
+  // 日志天数
+  CountDays: 50,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://xaiolu.top'
+    },
+  ],
+};
